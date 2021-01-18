b9158bcf61c2271946476055a090b33659ce8f57
Christos Panos <christos.a.panos@gmail.com>
2021-01-18 21:23:38 +0200

    add user repository

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -7,8 +7,8 @@
 -- IF spring.jpa.hibernate.ddl-auto IS SET TO create OR create-drop
 -- IT IS A Hibernate feature (nothing to do with Spring)
 
--- INSERT INTO APPUSERS (firstname, lastname, email, password, telephone, company) VALUES ('Admin', 'Admin', 'admin@mail.com', 'admin', '6955555555', 'Company1');
--- INSERT INTO APPUSERS (firstname, lastname, email, password, telephone, company) VALUES ('Christos', 'Papas', 'papas@mail.com', 'papas', '6954545454', 'Company2');
--- INSERT INTO APPUSERS (firstname, lastname, email, password, telephone, company) VALUES ('Christos', 'Panos', 'panos@mail.com', 'panos', '6978022548', 'Company3');
--- INSERT INTO APPUSERS (firstname, lastname, email, password, telephone, company) VALUES ('Panagiotis', 'Ioannou', 'ioannou@mail.com', 'ioannou', '6958881742', 'Company4');
--- INSERT INTO APPUSERS (firstname, lastname, email, password, telephone, company) VALUES ('Ioannis', 'Katridis', 'katridis@mail.com', 'katridis', '6942334158', 'Company5');
+INSERT INTO APPUSERS (firstname, lastname, email, password, telephone, company) VALUES ('Admin', 'Admin', 'admin@mail.com', 'admin', '6955555555', 'Company1');
+INSERT INTO APPUSERS (firstname, lastname, email, password, telephone, company) VALUES ('Christos', 'Papas', 'papas@mail.com', 'papas', '6954545454', 'Company2');
+INSERT INTO APPUSERS (firstname, lastname, email, password, telephone, company) VALUES ('Christos', 'Panos', 'panos@mail.com', 'panos', '6978022548', 'Company3');
+INSERT INTO APPUSERS (firstname, lastname, email, password, telephone, company) VALUES ('Panagiotis', 'Ioannou', 'ioannou@mail.com', 'ioannou', '6958881742', 'Company4');
+INSERT INTO APPUSERS (firstname, lastname, email, password, telephone, company) VALUES ('Ioannis', 'Katridis', 'katridis@mail.com', 'katridis', '6942334158', 'Company5');
