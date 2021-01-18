81f7414a44e72ddaa26109bec8f97e7de4751da9
Christos Panos <christos.a.panos@gmail.com>
2021-01-18 21:14:36 +0200

    add user domain

--- src/main/java/com/javaInterview/javaInterview/domains/User.java
+++ src/main/java/com/javaInterview/javaInterview/domains/User.java
@@ -4,18 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
-import javax.persistence.UniqueConstraint;
+import javax.persistence.*;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "USER", uniqueConstraints = {@UniqueConstraint(columnNames = "telephone")})
-
+@Table(name = "APPUSERS", uniqueConstraints = {@UniqueConstraint(columnNames = {"email","telephone"})})
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false)
+    private Long id;
 
     @Column(name = "firstname")
     private String firstName;
@@ -34,4 +35,60 @@ public class User {
 
     @Column(name = "company")
     private String company;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
 }
---
+++ src/main/resources/import.sql
@@ -0,0 +1,14 @@
+-- https://docs.spring.io/spring-boot/docs/current/reference/html/boot-features-sql.html
+-- https://docs.spring.io/spring-boot/docs/current/reference/html/howto-database-initialization.html
+-- IN THIS FILE WE CAN WRITE AN SQL SCRIPT CONTAINING:
+-- SCHEMA, TABLE AND DATA MANIPULATION QUERIES
+-- TO BE EXECUTED AUTOMATICALLY DURING THE INITIALIZATION OF THE APPLICATION
+-- AND AFTER THE CREATION OF SCHEMA AND TABLES BY Hibernate
+-- IF spring.jpa.hibernate.ddl-auto IS SET TO create OR create-drop
+-- IT IS A Hibernate feature (nothing to do with Spring)
+
+-- INSERT INTO APPUSERS (firstname, lastname, email, password, telephone, company) VALUES ('Admin', 'Admin', 'admin@mail.com', 'admin', '6955555555', 'Company1');
+-- INSERT INTO APPUSERS (firstname, lastname, email, password, telephone, company) VALUES ('Christos', 'Papas', 'papas@mail.com', 'papas', '6954545454', 'Company2');
+-- INSERT INTO APPUSERS (firstname, lastname, email, password, telephone, company) VALUES ('Christos', 'Panos', 'panos@mail.com', 'panos', '6978022548', 'Company3');
+-- INSERT INTO APPUSERS (firstname, lastname, email, password, telephone, company) VALUES ('Panagiotis', 'Ioannou', 'ioannou@mail.com', 'ioannou', '6958881742', 'Company4');
+-- INSERT INTO APPUSERS (firstname, lastname, email, password, telephone, company) VALUES ('Ioannis', 'Katridis', 'katridis@mail.com', 'katridis', '6942334158', 'Company5');
